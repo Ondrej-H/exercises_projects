4e090f13ba6041631bfc69140e76c67ba810eb04
Ondřej Hubert <ondrej.hubert@gmail.com>
2024-12-19 12:39:51 +0100

    add turtle - spiral

--- 03_cykly_zelva/07_spiral.py
+++ 03_cykly_zelva/07_spiral.py
@@ -1,17 +1,20 @@
 import turtle
 
-def draw_n_spiral(n, side=4, count_turns=57, direction="left"):
+
+def draw_spiral(n=100, side=0.01, count_turns=1000, side_rise=0.01, direction="left"):
     """
     Function draw spiral of n-gon shape.\n
     'n' is number of sides of the n-gon.\n
-    'side' is lenght of the first stroke from the center of the spiral.\n
+    'side' is length of the first stroke from the center of the spiral.\n
     'count_turns' is how many strokes will there be in total.\n
+    'side_rise' - the higher, the bigger the gaps between strokes.\n
     'direction' of rotation of the spiral must be string "left" or "right"
     """
     angle = (180 - (180 * (1 - 2/n)))
     t = turtle.Turtle()
-    t.speed(10)
-    t.pensize(2)
+    screen = turtle.Screen()
+    screen.tracer(0)
+    # t.pensize(2)
     t.left(90)
     for i in range(count_turns+1):
         t.forward(side)
@@ -19,8 +22,9 @@ def draw_n_spiral(n, side=4, count_turns=57, direction="left"):
             t.left(angle)       # for left-handed spiral
         else:
             t.right(angle)     # for right-handed spiral
-        side += 2
+        side += side_rise
+    screen.update()
     turtle.done()
 
 
-draw_n_spiral(8)
+draw_spiral()
